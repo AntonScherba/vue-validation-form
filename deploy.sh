221724bd91060dca7abca6c3316819c802775557
AntonScherba <antonscherba95@gmail.com>
2020-12-08 13:16:20 +0300

    add config

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:AntonScherba/vue-simon.git master:gh-pages
+git push -f git@github.com:AntonScherba/vue-validation-form.git master:gh-pages
 
 cd -
